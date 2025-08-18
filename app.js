550caba5b4267ad9f86901391aed7e2d9c9c3e8f
Dev-Waqas232 <devwaqas232@gmail.com>
2025-08-19 01:39:47 +0500

    feat: setup starter project

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "Hello World" });
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running");
+});
